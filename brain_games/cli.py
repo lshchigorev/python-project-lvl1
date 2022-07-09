fb948e40fe75d45f4ac3c3939fd64215c7ae011d
Leonid Shchigorev <l.shchigorev90@gmail.com>
2022-07-09 20:40:14 +0300

    up due to lint checker

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,4 +1,5 @@
 import prompt
 
+
 def welcome_user():
     name = prompt.string('May I have your name? ')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-#from brain_games import cli
+# from brain_games import cli
 from brain_games.cli import welcome_user
+
 
 def main():
     print('Welcome to the Brain Games!')
     welcome_user()
+
 
 if __name__ == 'main':
     main()
